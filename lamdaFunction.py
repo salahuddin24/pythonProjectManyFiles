c47695caf93ea7ccb9748dd83c1f2664ae177b3b
SALAH UDDIN <155283692+salahuddin24@users.noreply.github.com>
2024-10-10 12:40:39 +0600

    Add files via upload

---
+++ lamdaFunction.py
@@ -0,0 +1,43 @@
+## lambda function hocce amon akti function jar nam thakbe na
+# normal function
+# def calculate(a ,b) :
+#     return a*a + 2*a*b + b*b
+#
+# print(calculate(2,3))
+
+# lambda function
+# (lambda parameter : expression )(value) #syntex
+print((lambda a,b : a*a +2*a*b + b*b)(2,3))
+
+result = (lambda a,b : a*a +2*a*b + b*b)(2,3)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
