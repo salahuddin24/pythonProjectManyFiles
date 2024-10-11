d76ee4e252b346e08e496fe2ccc2905045e34345
SALAH UDDIN <155283692+salahuddin24@users.noreply.github.com>
2024-10-11 20:06:03 +0600

    Add files via upload

---
+++ comprehansiveList.py
@@ -0,0 +1,18 @@
+# systex
+# [expression for item in list]
+
+
+nums = [1, 3, 4, 5, 5,2]
+
+result = [x*x for x in nums]
+print(result)
+
+result2 = [x for x in nums if x%2==0]
+print(result2)
+
+
+
+
+
+
+
